b4c541b2c45b7d842785c68044d428ec76b82d46
Aurrus <167039299+Aurrus@users.noreply.github.com>
2024-04-14 20:54:09 -0700

    Add files via upload

---
+++ P1LAB1_GoodRobert.py
@@ -0,0 +1,8 @@
+# user enters first name
+first_name = input("Enter your first name: ")
+
+# user enters last name
+last_name = input("Enter your last name: ")
+
+# welcome message
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
